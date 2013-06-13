aab1357be44041a5d8337fee3d918c8b9293d6e5
cbello <Usuario1@Usuario1-PC>
2013-06-13 00:17:24 -0500

    Logic bussines on timer

--- src/java/com/dataejbopi/timer/LocalDateTimer.java
+++ src/java/com/dataejbopi/timer/LocalDateTimer.java
@@ -4,7 +4,10 @@
  */
 package com.dataejbopi.timer;
 
+import com.dataejbopi.entity.Payment;
+import com.dataejbopi.facade.PaymentFacade;
 import java.util.Date;
+import javax.ejb.EJB;
 import javax.ejb.Schedule;
 import javax.ejb.Singleton;
 import javax.ejb.LocalBean;
@@ -16,13 +19,28 @@ import javax.ejb.LocalBean;
 @Singleton
 @LocalBean
 public class LocalDateTimer {
+    @EJB
+    private PaymentFacade paymentFacade;
+    
     public Date localDate=new Date();
+    
     @Schedule(minute = "*", second = "*/10", dayOfMonth = "*", month = "*", year = "*", hour = "*", dayOfWeek = "*")
     public void myTimer() {
+        int lastMonth = localDate.getMonth();
         localDate.setDate(localDate.getDate()+1);
-        System.out.println(" ----------- New Day -----------");
-        System.out.println(localDate);
-        System.out.println(" -------------------------------");
+        if(lastMonth!=localDate.getMonth()){
+            System.out.println("--------- New Month ---------");
+            System.out.println(localDate);
+            System.out.println("-----------------------------");
+            
+        }
+        if(localDate.getDate()==2){
+            System.out.println("----- Update Extemporaneous Payment ----");
+            for(Payment payment:paymentFacade.findAll()){
+                paymentFacade.updatePaymentExtemporaneous(payment.getId());
+            }
+            System.out.println("-----------------------------------------");
+        }
     }
     // Add business logic below. (Right-click in editor and choose
     // "Insert Code > Add Business Method")
